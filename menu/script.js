1e3d902e104e67c1bfd872ed7334d9df8ded9318
NicoMantas <nicolasmantilla15062015@gmail.com>
2026-01-26 08:02:30 -0500

    Mejora en programa de menu

--- menu/script.js
+++ menu/script.js
@@ -1,6 +1,14 @@
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+
+    const target = document.querySelector(
+      link.getAttribute('href')
+    );
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
   });
 });
